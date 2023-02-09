e1c32f591e38a8000ce101d019fa3be937c6a6e1
unknown <marcinoles87@gmail.com>
2023-02-09 16:29:02 +0100

    add Temp component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import Temp from './components/Temp.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div>Temperatura</div>
+      <Temp></Temp>
+
     </div>
   );
 }
