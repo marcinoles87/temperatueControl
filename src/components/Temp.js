8c0341584549fb591a2b089b37ec1e0bd75cb26b
unknown <marcinoles87@gmail.com>
2023-02-13 16:45:33 +0100

    add new js

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -6,6 +6,7 @@ import '../components/temp.css'
 const Temp = () => {
 
     let tempBackground = document.querySelector('.temp')
+    const text = document.querySelector('#info')
 
     let [temp,setTemp] = useState(0);
 
@@ -14,6 +15,7 @@ const Temp = () => {
         if(temp >6) {
             tempBackground.classList.add('hot')
             tempBackground.classList.remove('cold')
+            text.textContent = "lekka kurtka + czapka"
 
         }
     
@@ -21,12 +23,14 @@ const Temp = () => {
             tempBackground.classList.add('veryHot')
             tempBackground.classList.remove('hot')
             tempBackground.classList.remove('cold')
+            text.textContent = "spodenki + koszulka :)"
 
         }
         if(temp < 0) {
             tempBackground.classList.add('cold')
             tempBackground.classList.remove('veryHot')
             tempBackground.classList.remove('hot')
+            text.textContent = "kurtka + czapka :)"
         }
     },[temp])
 
@@ -39,6 +43,7 @@ const Temp = () => {
             <h1>{temp} <span>&#176;</span>C</h1>
             <button onClick={() => setTemp(temp+1)}>+</button>
             <button onClick={() => setTemp(temp-1)}>-</button>
+            <p id="info"></p>
         </div>
      );
 }
